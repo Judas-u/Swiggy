065b3472b058b5f49a7a9462da19d60bb26f2560
Judas-u <adishwarraj9905@gmail.com>
2025-08-12 23:37:39 +0530

    Cards

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -20,17 +20,25 @@ export default function Header() {
                 <img className="h-110 w-60 absolute top-0 right-0" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/testing/seo-home/Sushi_replace.png"></img>
 
                 <div className="max-w-[60%] text-5xl text-white font-bold container mx-auto  text-center">
-                    OrderFood and Groceries. Discover Best Restaurants. Swiggy it!
+                    Order food & groceries. Discover best restaurants. Swiggy it!
                 </div>
 
-                <div className="max-w-[50%]  container mx-auto flex gap-5 mt-5">
+                <div className="max-w-[50%]  container mx-auto flex gap-5 mt-6">
                     <input className="bg-white w-[40%] text-xl px-6 py-4 rounded-2xl" placeholder="Delhi, India"></input>
                     <input className="bg-white w-[55%] text-xl px-6 py-4 rounded-2xl" placeholder="Search for Resturant, item or more"></input>
                 </div>
             </div>
 
-            <div>
-
+            <div className="max-w-[80%] container max-auto flex ml-40 ">
+                <a href="https://www.swiggy.com/restaurants">
+                    <img className="h-90 w-100" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/23/ec86a309-9b06-48e2-9adc-35753f06bc0a_Food3BU.png"></img>
+                </a>
+                <a href="https://www.swiggy.com/instamart?entryId=1234&entryName=mainTileEntry4&v=1">
+                    <img className="h-90 w-100" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/23/b5c57bbf-df54-4dad-95d1-62e3a7a8424d_IM3BU.png"></img>
+                </a>
+                <a href="https://www.swiggy.com/dineout">
+                <img className="h-90 w-100" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/23/b6d9b7ab-91c7-4f72-9bf2-fcd4ceec3537_DO3BU.png"></img>                  
+                </a>
             </div>
 
             
